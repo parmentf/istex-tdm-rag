f372f45006a9bde8c7b1302bb92d452d8d4474e0
François Parmentier <francois.parmentier@gmail.com>
2024-07-30 16:37:57 +0200

    feat: Avoid generation to dev quickly

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env bun
+
 import {
     RAGApplicationBuilder,
     Ollama,
@@ -29,13 +31,19 @@ const ragApplication = await new RAGApplicationBuilder()
 
 console.log(msee.parse('# POC RAG ISTEX TDM'));
 
-const answer = await ragApplication.query(
-    'Quels sont les différents types de services web disponibles?',
-);
-// console.log(answer);
-console.log(msee.parse(answer.content));
+if (false) {
+    const answer = await ragApplication.query(
+        'Quels sont les différents types de services web disponibles?',
+    );
+    // console.log(answer);
+    console.log(msee.parse(answer.content));
 
-console.log(msee.parse("## Sources"))
-for(let s of answer.sources) {
-    console.log(` - ${s.source}`);
+    console.log(msee.parse("## Sources"))
+    for (let s of answer.sources) {
+        console.log(` - ${s.source}`);
+    }
+} else {
+    const answer = await ragApplication.getContext('Quels sont les différents types de services web disponibles?');
+
+    console.log(answer);
 }
