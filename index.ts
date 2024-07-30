b0edd3f1b9e80daf30829587497eb6cb706a3ced
François Parmentier <francois.parmentier@gmail.com>
2024-07-30 16:49:11 +0200

    feat: Use script parameter as query

--- index.ts
+++ index.ts
@@ -29,12 +29,15 @@ const ragApplication = await new RAGApplicationBuilder()
     )
     .build();
 
+/////////////////////////////////////////////////
+const query = process.argv.slice(2).join(" ") || 'Quels sont les différents types de services web disponibles ?'
+
 console.log(msee.parse('# POC RAG ISTEX TDM'));
 
-if (false) {
-    const answer = await ragApplication.query(
-        'Quels sont les différents types de services web disponibles?',
-    );
+console.log(msee.parse(`## Question\n*${query}*`));
+
+if (true) {
+    const answer = await ragApplication.query(query);
     // console.log(answer);
     console.log(msee.parse(answer.content));
 
@@ -43,7 +46,7 @@ if (false) {
         console.log(` - ${s.source}`);
     }
 } else {
-    const answer = await ragApplication.getContext('Quels sont les différents types de services web disponibles?');
+    const answer = await ragApplication.getContext(query);
 
     console.log(answer);
 }
